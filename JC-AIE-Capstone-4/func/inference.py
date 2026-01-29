2f93790d5f63b27a841637a7a84c0a539a56b586
amjad-ahsan <203857485+amjad-ahsan@users.noreply.github.com>
2026-01-29 19:50:25 +0700

    Update inference.py

--- JC-AIE-Capstone-4/func/inference.py
+++ JC-AIE-Capstone-4/func/inference.py
@@ -1,54 +1,51 @@
-from ultralytics import YOLO
 from collections import defaultdict
 from func.calorie_map import get_calorie_info
 
-model = YOLO("model/best.pt")
+_model = None  # global cache
 
- # path to trained model
+
+def get_model():
+    """Load YOLO model only once (lazy load)."""
+    global _model
+    if _model is None:
+        from ultralytics import YOLO  # import here to avoid Streamlit startup crash
+        _model = YOLO("model/best.pt")
+    return _model
+
 
 def analyze_image(image_path, conf=0.25):
     """
     Food detection and calorie counting based on detected items only.
     """
+    model = get_model()
 
-    results = model(image_path, conf=conf, iou=0.6, imgsz=640)[0] # change imgsz if needed this also run the detektion using yolo
+    results = model(image_path, conf=conf, iou=0.6, imgsz=640)[0]
 
     foods = []
-
-    #for each box detected in the results/ keep info
     for box in results.boxes:
-        label = model.names[int(box.cls)] #give label
-        confidence = float(box.conf) #show confidence
+        label = model.names[int(box.cls)]
+        confidence = float(box.conf)
         foods.append((label, confidence))
 
     summary = defaultdict(lambda: {"count": 0, "conf": []})
-    #conf is confidence this is also confidence but threshold for filtering detections
     CONF_THRESHOLD = 0.4
 
     for label, conf_score in foods:
-        if conf_score < CONF_THRESHOLD: #make sure lower confidence dont affect calorie calculation
+        if conf_score < CONF_THRESHOLD:
             continue
         summary[label]["count"] += 1
         summary[label]["conf"].append(conf_score)
 
-    #Counting
     counts = {k: v["count"] for k, v in summary.items()}
     avg_conf = {k: sum(v["conf"]) / len(v["conf"]) for k, v in summary.items()}
 
     total_calories = 0
-
-            # Might give false estimation for very small or very large items but worth a try
-            # itwont display the rigid data number but give estimation based on portion size
-
-        #calories, unit = get_calorie_info(food)
-        #total_calories += calories * data["count"] * portion_factor
-
-        # Nvm since the data was to small , the protion factor resulted in the same exact answer as before
     for food, data in summary.items():
         calories, unit = get_calorie_info(food)
         total_calories += calories * data["count"]
 
     return counts, avg_conf, int(total_calories), results
+
 
 
 
